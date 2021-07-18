1280e3bad3c1468dab939c82af26e9d3af4af2ed
nima34366 <wickramasinghenlssck@gmail.com>
2021-07-18 14:38:05 +0530

    added grouped convolutions

--- team_code.py
+++ team_code.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Dense, MaxPooling1D, Conv1D, Dropout, MaxPoo
 from tensorflow.keras.models import Sequential
 from scipy.fft import rfft
 import tensorflow.keras.backend as K
+import os
 twelve_leads = ('I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6')
 six_leads = ('I', 'II', 'III', 'aVR', 'aVL', 'aVF')
 four_leads = ('I', 'II', 'III', 'V2')
@@ -62,19 +63,6 @@ def training_code(data_directory, model_directory):
 
     def train_model(leads):
         
-        def indices(leads):
-            if leads==twelve_leads:
-                pos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-            elif leads==six_leads:
-                pos = [0,1,2,3,4,5,12,13,14,15,16,17]
-            elif leads==four_leads:
-                pos = [0,1,2,7,12,13,14,19]
-            elif leads==three_leads:
-                pos = [0,1,7,12,13,19]
-            else:
-                pos = [0,1,12,13]
-            return pos
-
         def parse_example(serialized, image_shape=(1024, 24),label_shape=(26,)):
             features = {'image': tf.io.FixedLenFeature([], tf.string), 'label': tf.io.FixedLenFeature([], tf.string)}
             parsed_example = tf.io.parse_single_example(serialized=serialized, features=features)
@@ -108,10 +96,12 @@ def training_code(data_directory, model_directory):
     for leads in lead_sets:
         print('Training model'+str(len(leads)))
         train_model(leads)
+    
+    os.system('rm processed*')
 
 def run_model(model, header, recording):
 
-    def generate_image(recording,frequency):
+    def generate_image(recording,frequency,leads):
         recording = recording[:,:20*frequency]
         fftsignals = rfft(recording)
         fftsignals = fftsignals[:,:1024]
@@ -120,6 +110,7 @@ def run_model(model, header, recording):
         angle = np.angle(fftsignals)
         image = np.vstack((mag,angle))
         image = np.transpose(image,(1,0)).astype(np.float32)
+        image = np.take(image,indices_predicting(leads),axis=-1)
         return image
     classes=np.array(["164889003","164890007","6374002","426627000","733534002","713427006","270492004","713426002","39732003","445118002","251146004","698252002","426783006","284470004","10370003","365413008","427172004","164947007","111975006","164917005","47665007","427393009","426177001","427084000","164934002","59931005"])
     frequency=int(get_frequency(header))
@@ -131,10 +122,10 @@ def run_model(model, header, recording):
     recording = (recording - baselines)/adc_gains
     if num_samples<(22*frequency):
         recording = np.hstack((recording,np.zeros((len(leads),22*frequency-num_samples))))
-    image = np.array([generate_image(recording,frequency)])
+    image = np.array([generate_image(recording,frequency,leads)])
     probabilities = model.predict(image)
     probabilities = probabilities.reshape(26)
-    labels = (probabilities>0.2)*1
+    labels = (probabilities>0.3)*1
 
     return classes, labels, probabilities
 
@@ -148,15 +139,15 @@ def load_model(model_directory, leads):
 def create_model(leads):
     
     model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=7, activation='relu', input_shape=(1024,2*len(leads))))
-    model.add(Conv1D(filters=64, kernel_size=7, activation='relu'))
+    model.add(Conv1D(filters=72, groups=len(leads), kernel_size=7, activation='relu', input_shape=(1024,2*len(leads))))
+    model.add(Conv1D(filters=72, groups=len(leads), kernel_size=7, activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=128, kernel_size=9, activation='relu'))
-    model.add(Conv1D(filters=128, kernel_size=9, activation='relu'))
-    model.add(Dropout(0.1))
+    model.add(Conv1D(filters=144, groups=len(leads), kernel_size=9, activation='relu'))
+    model.add(Conv1D(filters=144, groups=len(leads), kernel_size=9, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=256, kernel_size=11, activation='relu'))
-    model.add(Conv1D(filters=256, kernel_size=11, activation='relu'))
+    model.add(Conv1D(filters=288, groups=len(leads), kernel_size=11, activation='relu'))
+    model.add(Conv1D(filters=288, groups=len(leads), kernel_size=11, activation='relu'))
     model.add(Dropout(0.3))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Flatten())
@@ -169,3 +160,29 @@ def create_model(leads):
     model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy', AUROC, AUPRC])
     model.summary()
     return model
+
+def indices(leads):
+    if leads==twelve_leads:
+        pos = [0,12,1,13,2,14,3,15,4,16,5,17,6,18,7,19,8,20,9,21,10,22,11,23]
+    elif leads==six_leads:
+        pos = [0,12,1,13,2,14,3,15,4,16,5,17]
+    elif leads==four_leads:
+        pos = [0,12,1,13,2,14,7,19]
+    elif leads==three_leads:
+        pos = [0,12,1,13,7,19]
+    else:
+        pos = [0,12,1,13]
+    return pos
+
+def indices_predicting(leads):
+    if leads==twelve_leads:
+        pos = [0,12,1,13,2,14,3,15,4,16,5,17,6,18,7,19,8,20,9,21,10,22,11,23]
+    elif leads==six_leads:
+        pos = [0,6,1,7,2,8,3,9,4,10,5,11]
+    elif leads==four_leads:
+        pos = [0,4,1,5,2,6,3,7]
+    elif leads==three_leads:
+        pos = [0,3,1,4,2,5]
+    else:
+        pos = [0,2,1,3]
+    return pos
